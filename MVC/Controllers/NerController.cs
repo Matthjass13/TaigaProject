9163f82927c29e36571ccfa3de6d973faa0971fa
Nathan Python <nathan.python@hes-so.ch>
2025-10-26 19:27:00 +0100

    feat : US19 : As a user, I want to see the level of each energy type, so that I know which ones are importants

---
+++ MVC/Controllers/NerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class NerController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ MVC/Models/EnergyLevel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MVC.Models
+{
+    public class EnergyLevel
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Value { get; set; }
+    }
+}
