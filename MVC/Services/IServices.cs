5ed41748f49b2ba94f91deaf9158c73654a9974b
Matthjass <gaillardmatthias1.9991@gmail.com>
2025-10-26 11:31:57 +0100

    Completed US 32 and created views file architecture

---
+++ MVC/Services/IServices.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace MVC.Services
+{
+    public interface IDigitecServices
+    {
+
+
+
+    }
+}
---
+++ MVC/Services/Services.cs
@@ -0,0 +1,24 @@
+﻿
+//using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using MVC.Services;
+
+namespace MVC.Services
+{
+    public class Services //: IServices
+    {
+
+        private readonly HttpClient _client;
+        private readonly string _baseUrl = "https://localhost:7003/api/Digitec";
+
+        public Services(HttpClient client)
+        {
+            _client = client;
+        }
+
+
+    }
+
+}
