86bd48de700d72617599adeeb0da76f474f37953
Nathan Python <nathan.python@hes-so.ch>
2025-11-06 14:19:53 +0100

    us-nathan

--- ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
+++ ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3edab6fd5fb1f1b9e621401214b40e43351760a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a64f1dcae7d51c130d48e2382f751c7bcd85f84")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
