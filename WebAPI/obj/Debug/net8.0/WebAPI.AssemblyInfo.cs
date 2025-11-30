9eee0ae1833e4f96ef5a0a5ec61dac85ff4d94b7
sarapereirad <sara.pereirad@hes-so.ch>
2025-11-30 17:52:55 +0100

    push2

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36e2d8a63f92c8f84ec6cf2894f11f3fcda759e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7af24744ee781f3ed0183e2cb87648d99b94bc21")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
