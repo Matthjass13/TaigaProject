499868865dec267ac1794be844c3c336ba05aecb
sarapereirad <sara.pereirad@hes-so.ch>
2025-12-09 20:49:36 +0100

    Test unitaires - mvc 2

--- MVCTest/UnitTest1.cs
+++ MVCTest/UnitTest1.cs
@@ -82,6 +82,66 @@ namespace MVCTest
             serviceMock.Verify(s => s.GetPvChartAsync(), Times.Once);
         }
 
+        [Fact]
+        public void NER_ReturnsCorrectView()
+        {
+            var serviceMock = new Mock<IValaisServices>();
+            var controller = new HomeController(serviceMock.Object);
+
+            var result = controller.NER();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("~/Views/Home/NER/NER.cshtml", viewResult.ViewName);
+        }
+
+        [Fact]
+        public void Biogaz_ReturnsCorrectView()
+        {
+            var serviceMock = new Mock<IValaisServices>();
+            var controller = new HomeController(serviceMock.Object);
+
+            var result = controller.Biogaz();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("~/Views/Home/NER/Biogaz/Biogaz.cshtml", viewResult.ViewName);
+        }
+
+        [Fact]
+        public void Eolien_ReturnsCorrectView()
+        {
+            var serviceMock = new Mock<IValaisServices>();
+            var controller = new HomeController(serviceMock.Object);
+
+            var result = controller.Eolien();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("~/Views/Home/NER/Eolien/Eolien.cshtml", viewResult.ViewName);
+        }
+
+        [Fact]
+        public void MiniHydraulique_ReturnsCorrectView()
+        {
+            var serviceMock = new Mock<IValaisServices>();
+            var controller = new HomeController(serviceMock.Object);
+
+            var result = controller.MiniHydraulique();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("~/Views/Home/NER/Mini-hydraulique/MiniHydraulique.cshtml", viewResult.ViewName);
+        }
+
+        [Fact]
+        public void Privacy_ReturnsView()
+        {
+            var serviceMock = new Mock<IValaisServices>();
+            var controller = new HomeController(serviceMock.Object);
+
+            var result = controller.Privacy();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+
     }
 }
 
